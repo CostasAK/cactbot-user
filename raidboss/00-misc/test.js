8e5c7c1ca6a13893f176fc36fbfe2dd19a808c26
CostasAK <costas@kokke.eu>
2022-01-02 14:08:14 +0100

    fixed sound volumes, enables and tests

--- raidboss/00-misc/test.js
+++ raidboss/00-misc/test.js
@@ -1,22 +1,26 @@
 'use strict'
 
 // eslint-disable-next-line no-undef
-Options.Triggers = Options.Triggers.concat([{
-  zoneRegex: /.*/,
+Options.Triggers = Options.Triggers.concat([
+  {
+    zoneRegex: /.*/,
 
-  triggers: [{
-    id: 'Test Sounds',
-    regex: /Ready to unleash Eternal Breath/,
-    sound: '../../user/sounds/tsunami-drop.wav',
-    soundVolume: 0,
-    delaySeconds: 0
-  }]
-},
+    triggers: [
+      {
+        id: "Test Sounds",
+        regex: /Ready to unleash Eternal Breath/,
+        sound: "../../user/wav/tsunami-drop.wav",
+        infoText: "Tsunami!",
+        soundVolume: 1,
+        delaySeconds: 0,
+      },
+    ],
+  },
 
-// Pre-raid Check
-{
-  zoneRegex: /Barracks$/,
-  timeline: `
+  // Pre-raid Check
+  {
+    zoneRegex: /Barracks$/,
+    timeline: `
     0 "--Reset--" sync /You bid farewell to the flame squadron sergeant/ window 10000 jump 0
     0 "Start" sync /:You bow courteously to the flame squadron sergeant/ window 0,1
     0 "--Reset--" sync /You bid farewell to the serpent squadron sergeant/ window 10000 jump 0
@@ -24,17 +28,23 @@ Options.Triggers = Options.Triggers.concat([{
     3 "Double Call-outs Test"
   `,
 
-  timelineTriggers: [{
-    id: 'Double Test',
-    regex: /(Double Call-outs Test)/,
-    infoText: 'Please tell me if I am repeating myself',
-    tts: 'Testing testing'
-  }]
-}
-])
+    timelineTriggers: [
+      {
+        id: "Double Test",
+        regex: /(Double Call-outs Test)/,
+        infoText: "Please tell me if I am repeating myself",
+        tts: "Testing testing",
+      },
+    ],
+  },
+]);
 
 // eslint-disable-next-line no-undef
 Object.assign(Options.PerTriggerOptions, {
+  "Test Sounds": {
+    SoundAlert: true,
+  },
+
   "Test Poke": {
     SpeechAlert: false,
     SoundAlert: true,
@@ -47,7 +57,7 @@ Object.assign(Options.PerTriggerOptions, {
       "C:/Program Files (x86)/Advanced Combat Tracker/Hojoring/resources/wav/Agro.wav",
   },
 
-  // 'Double Test': {
-  //   SpeechAlert: true
-  // }
+  "Double Test": {
+    SpeechAlert: true,
+  },
 });
