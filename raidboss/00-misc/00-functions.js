692f10086bac57853d722db6e80835f6d7bd0715
CostasAK <costas@kokke.eu>
2022-01-14 20:47:09 +0100

    global user functions, for random array elements with or without chances

---
+++ raidboss/00-misc/00-functions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Options.UserFunctions = {
+  random_integer: (num) => Math.floor(Math.random() * num),
+};
+
+Object.assign(Options.UserFunctions, {
+  random_array_element: (arr) => {
+    if (
+      arr.every((e) => e.length === 2 && Number.isInteger(e[1]) && e[1] >= 0)
+    ) {
+      const chances = arr.map((e, i) => Array(e[1]).fill(i)).flat(1);
+      return arr[
+        chances[Options.UserFunctions.random_integer(chances.length)]
+      ][0];
+    }
+    return arr[Options.UserFunctions.random_integer(arr.length)];
+  },
+});
