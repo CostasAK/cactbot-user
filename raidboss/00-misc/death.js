06ed1db24147acc0c6386d530fb3c3dc5115c6b5
CostasAK <costas@kokke.eu>
2022-01-14 21:05:04 +0100

    flag to disable all death sounds, implemented ahmeds tymos maris shiroes gusts yukis new sounds,

--- raidboss/00-misc/death.js
+++ raidboss/00-misc/death.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const death_sounds_enabled = true;
+
+var mari_death_counter = 0;
+
 Options.Triggers.push({
   zoneRegex: /.*/,
   triggers: [
@@ -14,6 +18,7 @@ Options.Triggers.push({
     },
     {
       id: "Death Ahmed",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Ahmed Sins",
@@ -21,13 +26,24 @@ Options.Triggers.push({
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
-      sound: "../../user/wav/roblox-death-sound.wav",
+      sound: () =>
+        "../../user/wav/Ahmed2022-01-11/0" +
+        Options.UserFunctions.random_array_element([
+          ["1", 6],
+          ["2", 5],
+          ["3", 4],
+          ["4", 3],
+          ["5", 2],
+          ["6", 1],
+        ]) +
+        ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Tymo",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Tymo Aimali",
@@ -35,13 +51,17 @@ Options.Triggers.push({
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
-      sound: "../../user/wav/roblox-death-sound.wav",
+      sound: () =>
+        "../../user/wav/Tymo2022-01-11/" +
+        Options.UserFunctions.random_integer(28) +
+        ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Marielle",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Marielle Maru",
@@ -49,13 +69,16 @@ Options.Triggers.push({
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
-      sound: "../../user/wav/roblox-death-sound.wav",
+      sound: () =>
+        "../../user/wav/Mari2022-01-12/" + mari_death_counter + ".wav",
+      run: () => (mari_death_counter = (mari_death_counter + 1) % 3),
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Kali",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Kali Sunfeather",
@@ -68,11 +91,12 @@ Options.Triggers.push({
         Math.floor(Math.random() * 10) +
         ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Tali",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Tali Lamora",
@@ -85,28 +109,30 @@ Options.Triggers.push({
         Math.floor(Math.random() * 6) +
         ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Shiroe",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
-        target: "Shiroe Enchanter",
+        target: "Shiroe Enchanter|Roe Two",
         capture: false,
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
       sound: () =>
-        "../../user/wav/Shiro2022-01-10/Shiroe-0" +
-        (Math.floor(Math.random() * 4) + 1) +
+        "../../user/wav/Shiroe2022-01-10/Shiroe-0" +
+        Math.floor(Math.random() * 9) +
         ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Gust",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Gust Leonard",
@@ -114,13 +140,26 @@ Options.Triggers.push({
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
-      sound: "../../user/wav/roblox-death-sound.wav",
+      sound: () =>
+        "../../user/wav/Gust2022-01-12/" +
+        Options.UserFunctions.random_array_element([
+          "Back to work",
+          "How dissapointing",
+          "How intriguing",
+          "It's size",
+          "No no no",
+          "Pathetic thing",
+          "Personal development",
+          "Try again",
+        ]) +
+        ".wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
     {
       id: "Death Yuki",
+      disabled: !death_sounds_enabled,
       type: "WasDefeated",
       netRegex: NetRegexes.wasDefeated({
         target: "Yuki Kimura",
@@ -128,9 +167,9 @@ Options.Triggers.push({
       }),
       condition: (data) => !data.death_cooldown,
       preRun: (data) => (data.death_cooldown = true),
-      sound: "../../user/wav/roblox-death-sound.wav",
+      sound: "../../user/wav/oh-no-knuckles.wav",
       options: {
-        SoundAlert: false,
+        SoundAlert: true,
       },
     },
   ],
