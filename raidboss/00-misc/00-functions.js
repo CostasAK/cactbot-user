8c85ea47b14f2e16f0dbc72c6031f3ae22a77541
CostasAK <costas@kokke.eu>
2022-01-19 17:00:20 +0100

    declare functions directly in options instead of assigning

--- raidboss/00-misc/00-functions.js
+++ raidboss/00-misc/00-functions.js
@@ -1,19 +1,17 @@
 "use strict";
 
-Options.UserFunctions = {
-  random_integer: (num) => Math.floor(Math.random() * num),
-};
+if (!Options.UserFunctions) {
+  Options.UserFunctions = {};
+}
 
-Object.assign(Options.UserFunctions, {
-  random_array_element: (arr) => {
-    if (
-      arr.every((e) => e.length === 2 && Number.isInteger(e[1]) && e[1] >= 0)
-    ) {
-      const chances = arr.map((e, i) => Array(e[1]).fill(i)).flat(1);
-      return arr[
-        chances[Options.UserFunctions.random_integer(chances.length)]
-      ][0];
-    }
-    return arr[Options.UserFunctions.random_integer(arr.length)];
-  },
-});
+Options.UserFunctions.random_integer = (num) => Math.floor(Math.random() * num);
+
+Options.UserFunctions.random_array_element = (arr) => {
+  if (arr.every((e) => e.length === 2 && Number.isInteger(e[1]) && e[1] >= 0)) {
+    const chances = arr.map((e, i) => Array(e[1]).fill(i)).flat(1);
+    return arr[
+      chances[Options.UserFunctions.random_integer(chances.length)]
+    ][0];
+  }
+  return arr[Options.UserFunctions.random_integer(arr.length)];
+};
